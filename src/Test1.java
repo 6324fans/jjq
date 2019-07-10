b965ac24c1ada74e16b1a62dec227422553b3fd8
jjq <281136113@qq.com>
2019-07-10 15:10:41 +0800

    bbb

--- src/Test1.java
+++ src/Test1.java
@@ -10,6 +10,7 @@ public class Test1 {
 
     public String doA(){
         String s = UUID.randomUUID().toString();
+        //修改啊
         System.out.println(s+"|"+s.length());
         s=s.substring(0, 8) + s.substring(9, 13) + s.substring(14, 18) + s.substring(19, 23) + s.substring(24);
 
